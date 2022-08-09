2dd057efba1bc0b979595b0ccdd905b6f022e35c
Bruno Alves <40463751+LeGoonie@users.noreply.github.com>
2022-08-09 23:34:32 +0000

    remove string

--- MarketAnalyzer.Data/JsonObjects/Login.cs
+++ MarketAnalyzer.Data/JsonObjects/Login.cs
@@ -7,5 +7,3 @@
         public Token result { get; set; }
     }
 }
-
-//{"code":0,"result":{"token":"grL0gNYoMoLUB1ZoAKLfhXkoMoLODiO1WoL9.grLtk3PoMoLmqFEsMasbNK9fkXudkNBtR2jpkr5dINZoAKLtRNZoMlG1MQZ3PJR3MJZcOpEfqXGoMwcoqNWaka9tIKO6OlD2PlOoAKLahSVthKO6Olqph3BfIGHskp92INqfyNPdgFa6OosokXVik3qbkpZoMoO3Ip91RSEYkFLbqpB3RSaiAwf5goOcOwHryNIthXBwICO6PKsokpBwyS9dDFLtqoO6grLBDrO6PCsoZ0GoMlH9vN0.ppgH4XHRXjtUpM2n83Aycyz23GVirT-WRcxRof0zHOc","user":{"id":"5622","fullname":"Victor F","email":"7fouad@provewais.xyz","privilege":0,"trialEligibility":{"isEligibleForPremiumPlanTrial":1},"excelAddinKey":"","public_api_key":"","joinDate":"2020-03-20 09:48:33","firstName":"Victor"}}}
